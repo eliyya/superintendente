96be3f900a820d9550687dbd90000f9f2f86d219
eliyya <tecno_eli@hotmail.com>
2023-09-12 15:45:20 -0600

    sett port

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ app.get('/webhook/twitch/:ID', (req, res) => {
     res.send('ok')
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ?? 3000, () => {
     console.log('Listening on port 3000')
 })
 
